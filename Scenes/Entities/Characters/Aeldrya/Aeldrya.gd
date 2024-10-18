d38d652b6a1cd94da2f1ca8949be36036f2e99fa
jgascart <j.gascart@gmail.com>
2024-10-18 20:22:35 +0200

    Adding player movement

---
+++ Scenes/Entities/Characters/Aeldrya/Aeldrya.gd
@@ -0,0 +1,15 @@
+extends CharacterBody2D
+
+const SPEED = 200
+var move_direction = Vector2(0,0)
+
+func _physics_process(_delta: float) -> void:
+	move()
+
+func move() -> void:
+	move_direction.x = int(Input.is_action_pressed("move_right")) - int(Input.is_action_pressed("move_left"))
+	move_direction.y = int(Input.is_action_pressed("move_down")) - int(Input.is_action_pressed("move_up"))
+	
+	velocity = move_direction.normalized() * SPEED
+	
+	move_and_slide()
---
+++ Scenes/Entities/Characters/Aeldrya/AeldryaAnimations.gd
@@ -0,0 +1,36 @@
+extends AnimatedSprite2D
+
+var move_animation : String = "idle_down"
+
+func _process(delta: float) -> void:
+	var move_direction = get_parent().move_direction
+	
+	match move_direction:
+		Vector2(-1, 0):
+			move_animation = "run_left"
+		Vector2(-1, -1):
+			move_animation = "run_up_left"
+		Vector2(-1, 1):
+			move_animation = "run_down_left"
+		Vector2(0, -1):
+			move_animation = "run_up"
+		Vector2(0, 0):
+			match move_animation:
+				"run_down" : move_animation = "idle_down"
+				"run_down_left" : move_animation = "idle_down_left"
+				"run_down_right" : move_animation = "idle_down_right"
+				"run_left" : move_animation = "idle_left"
+				"run_right" : move_animation = "idle_right"
+				"run_up" : move_animation = "idle_up"
+				"run_up_left" : move_animation = "idle_up_left"
+				"run_up_right" : move_animation = "idle_up_right"
+		Vector2(0, 1):
+			move_animation = "run_down"
+		Vector2(1, -1):
+			move_animation = "run_up_right"
+		Vector2(1, 0):
+			move_animation = "run_right"
+		Vector2(1,1):
+			move_direction = "run_down_right"
+		
+	play(move_animation)
